d5e184d96836876d7d0107c43ad88177fdfc7bbc
Iana <yana.kolchanova@gmail.com>
2017-01-29 12:48:47 +0300

    money information

--- public/src/dish/dish-list.component.js
+++ public/src/dish/dish-list.component.js
@@ -2,13 +2,22 @@ angular
     .module("DroneCafeApp")
     .component("dishList", {
         templateUrl: '/src/dish/dish-list.html',
-        controller: function(DishService, AuthService, $state){
+        controller: function(DishService, AuthService, $state, $sessionStorage){
             var vm = this;
 
             vm.limit = 12;
             vm.getDishList = getDishList;
             vm.formatList = formatList;
 
+            vm.isAvailable = function(dishPrice){
+                return $sessionStorage.user.balance >= dishPrice;
+            };
+
+            vm.getEnoughMoney = function(dishPrice){
+                return dishPrice - $sessionStorage.user.balance;
+            };
+
+            //todo возможно надо убрать AuthServiec или сделать там геттер для получения user
             if (!AuthService.isAuthorized())
                 $state.go("login");
 

026b6ef528f001dbc71aaeed1ddd699a2e18ba5d
Iana <yana.kolchanova@gmail.com>
2017-02-19 12:08:05 +0300

    remove comments

--- server/lib/socket.js
+++ server/lib/socket.js
@@ -3,8 +3,6 @@ module.exports = function (server) {
     let kitchenIO = io.of("/kitchen");
     let clientIO = io.of("/client");
 
-    //kitchenIO.on("connection", function (socket) {});
-
     clientIO.on("connection", function (socket) {
         socket.on("newConnect", function (data) {
             socket.join(data.userID);
